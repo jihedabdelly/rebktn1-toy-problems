faf6e4cef182b622b885338f3eeac9ce7bde5f05
jihedabdelly <jihednew7@gmail.com>
2020-02-18 09:26:34 +0100

    words within words

--- wordsWithinWords/wordsWithinWords.js
+++ wordsWithinWords/wordsWithinWords.js
@@ -9,4 +9,26 @@ nestedWordCount([ "gray", "grays", "ray", "rays", "strays" ]) // 'grays'
 
 function nestedWordCount(words) {
   // your code here...
+  var obj = {}
+  var ndxOfMax = 0;
+
+  for (var i = 0; i < words.length; i++) {
+  	obj[i] = 0
+  }
+
+  for (var i = 0; i < words.length; i++) {
+  	for (var j = 0; j < words.length; j++) {
+  		if(words[i].includes(words[j])) {
+  			obj[i]++
+  		}	
+  	}
+  }
+
+  for (var i = 0; i < words.length; i++) {
+  	if(obj[i]>obj[ndxOfMax]) {
+  		ndxOfMax = i
+  	}
+  }
+
+  return words[ndxOfMax]
 }
